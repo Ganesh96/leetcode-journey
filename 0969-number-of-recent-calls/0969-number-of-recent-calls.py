bce298eeb3aebf446f8f9cd3dfffc315bf0eb581
GS <ganesan.santhanam96@gmail.com>
2024-12-13 17:59:34 -0600

    Time: 33 ms (76.4%), Space: 22.2 MB (47.15%) - LeetHub

--- 0969-number-of-recent-calls/0969-number-of-recent-calls.py
+++ 0969-number-of-recent-calls/0969-number-of-recent-calls.py
@@ -1,24 +1,17 @@
-from collections import deque
-
 class RecentCounter:
+
     def __init__(self):
-        """
-        Initializes the counter with zero recent requests.
-        """
-        self.requests = deque()  # Use a deque to store requests
+        self.slide_window = deque()
+        
 
     def ping(self, t: int) -> int:
-        """
-        Adds a new request at time t and returns the number of requests 
-        that have happened in the past 3000 milliseconds (including the new request).
-        """
-        self.requests.append(t)  # Add the new request time
+        self.slide_window.append(t)
 
-        # Remove requests older than 3000 milliseconds
-        while self.requests and self.requests[0] < t - 3000:
-            self.requests.popleft()
+        while(self.slide_window[0] < t - 3000):
+            self.slide_window.popleft()
 
-        return len(self.requests)  # Return the count of recent requests
+        return len(self.slide_window)
+        
 
 
 # Your RecentCounter object will be instantiated and called as such:
