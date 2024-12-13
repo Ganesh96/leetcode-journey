17a4a189b8662feaa03c96d8cc41a20160712a9a
GS <ganesan.santhanam96@gmail.com>
2024-12-13 17:27:39 -0600

    Time: 1095 ms (15.86%), Space: 22 MB (57.03%) - LeetHub

--- 0969-number-of-recent-calls/0969-number-of-recent-calls.py
+++ 0969-number-of-recent-calls/0969-number-of-recent-calls.py
@@ -5,20 +5,33 @@ class RecentCounter:
         
 
     def ping(self, t: int) -> int:
-        self.recent_requests.append(t)
-
+        self.recent_requests = [t] + self.recent_requests
         anchor = 0
         lower_limit = t-3000
 
         if lower_limit > 0:
             for time in self.recent_requests:
-                if time<lower_limit:
+                if time >= lower_limit:
                     anchor+=1
                 else:
                     break
-            del self.recent_requests[:anchor]
+            del self.recent_requests[anchor:]
 
         return len(self.recent_requests)
+
+        #v1
+        # anchor = 0
+        # lower_limit = t-3000
+
+        # if lower_limit > 0:
+        #     for time in self.recent_requests:
+        #         if time<lower_limit:
+        #             anchor+=1
+        #         else:
+        #             break
+        #     del self.recent_requests[:anchor]
+
+        # return len(self.recent_requests)
 
     
 # Your RecentCounter object will be instantiated and called as such:
