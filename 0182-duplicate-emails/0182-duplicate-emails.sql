bf1aba8d44f77dced5acf86f0fed004b5476cb42
GS <ganesan.santhanam96@gmail.com>
2024-12-14 21:21:02 -0600

    Time: 939 ms (6.01%), Space: 0B (100%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,5 +1,12 @@
+-- /* Write your T-SQL query statement below */
+-- select email as "Email"
+-- from Person
+-- group by email
+-- having count(1)>1
 /* Write your T-SQL query statement below */
-select email as "Email"
-from Person
-group by email
-having count(1)>1
+WITH EmailCount as (
+    SELECT email, count(*) as eCount from Person
+    GROUP BY email
+), x as (SELECT email as Email
+from EmailCount where eCount > 1) 
+select x.Email from x
