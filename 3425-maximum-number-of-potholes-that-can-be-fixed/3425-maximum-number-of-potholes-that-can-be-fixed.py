893ab451c6132fa8cf72cd28682eb7c8608b2ea2
GS <ganesan.santhanam96@gmail.com>
2024-12-15 20:48:47 -0600

    Time: 287 ms (52.55%), Space: 18.1 MB (48.18%) - LeetHub

---
+++ 3425-maximum-number-of-potholes-that-can-be-fixed/3425-maximum-number-of-potholes-that-can-be-fixed.py
@@ -0,0 +1,34 @@
+import heapq
+
+class Solution:
+    def maxPotholes(self, road: str, budget: int) -> int:
+        potholes = list()
+        series = 0
+        for c in road:
+            if c == 'x':
+                series += 1
+            else:
+                if series != 0:
+                    heapq.heappush(potholes, -series)
+                    series = 0
+
+        if series != 0:  # Handle the last series of potholes
+            heapq.heappush(potholes, -series)
+        print(potholes)
+
+        repaired_potholes = 0
+        while potholes:
+            block_size = -heapq.heappop(potholes)
+            repair_cost = block_size + 1
+            if repair_cost <= budget:
+                budget -= repair_cost
+                repaired_potholes += block_size
+            else:
+                max_k = min(block_size, budget - 1)
+                if max_k <= 0:
+                    break
+                repaired_potholes += max_k
+                budget -= (max_k + 1) 
+                break 
+
+        return repaired_potholes
