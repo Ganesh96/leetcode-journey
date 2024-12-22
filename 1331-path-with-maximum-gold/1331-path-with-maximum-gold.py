e72020041bfda1f268b875939a365d98fe36193c
GS <ganesan.santhanam96@gmail.com>
2024-12-22 17:29:41 -0600

    Time: 5674 ms (5.5%), Space: 17.7 MB (6.25%) - LeetHub

--- 1331-path-with-maximum-gold/1331-path-with-maximum-gold.py
+++ 1331-path-with-maximum-gold/1331-path-with-maximum-gold.py
@@ -1,26 +1,23 @@
 class Solution:
     def getMaximumGold(self, grid: List[List[int]]) -> int:
         rows, cols = len(grid), len(grid[0])
-        def dfs(r,c):
-            if min(r,c)<0 or r == rows or c==cols or grid[r][c]==0:# or (r,c) in visit:
+        def dfs(r,c,visit):
+            if min(r,c)<0 or r == rows or c==cols or grid[r][c]==0 or (r,c) in visit:
                 return 0
             
-            #visit.add((r,c))
-            aux = grid[r][c]
-            grid[r][c] = 0
-            res= 0
+            visit.add((r,c))
+            gold_collected = 0
             near = [(1,0),(-1,0),(0,1),(0,-1)]
             for dr,dc in near:
-                res = max(res,aux + dfs(r+dr,c+dc))
-            #visit.remove((r,c))
-            grid[r][c] = aux
-            return res
+                gold_collected = max(gold_collected,grid[r][c] + dfs(r+dr,c+dc,visit))
+            visit.remove((r,c))
+            return gold_collected
         
         res = 0
 
         for r in range(rows):
             for c in range(cols):
                 visited = set()
-                res = max(res,dfs(r,c))
+                res = max(res,dfs(r,c,visited))
 
         return res
