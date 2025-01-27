448ac4cf2fb1d6ea3dcb0ed307ed6d08fa2ad450
GS <ganesan.santhanam96@gmail.com>
2025-01-27 12:36:53 -0500

    Time: 3 ms (83.38%), Space: 18 MB (18.34%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,20 +1,9 @@
-class Solution:
-    def isValidSudoku(self, board: List[List[str]]) -> bool:
-        cols = collections.defaultdict(set)
-        rows = collections.defaultdict(set)
-        squares = collections.defaultdict(set)
-        for r in range(9):
-            for c in range(9):
-                if board[r][c] == ".":
-                    continue
-                R = board[r][c] in rows[r]
-                C = board[r][c] in cols[c]
-                S = board[r][c] in squares[(r//3,c//3)]
-                if R or C or S:
-                    return False
-                cols[c].add(board[r][c])
-                rows[r].add(board[r][c])
-                squares[(r//3,c//3)].add(board[r][c])
-        return True
-
-        
+class Solution(object):
+    def isValidSudoku(self, board):
+        res = []
+        for i in range(9):
+            for j in range(9):
+                element = board[i][j]
+                if element != '.':
+                    res += [(i, element), (element, j), (i // 3, j // 3, element)]
+        return len(res) == len(set(res))
