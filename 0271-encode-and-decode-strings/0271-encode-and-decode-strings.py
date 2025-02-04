ab10669d5e2abb987659680388d5f85b19a8b663
GS <ganesan.santhanam96@gmail.com>
2025-02-04 11:46:56 -0500

    Time: 71 ms (55.78%), Space: 18.2 MB (53.71%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -1,6 +1,36 @@
 class Codec:
-    def encode(self, strs):
-        return '\U0001f926‍♂️'.join(strs)
+    def encode(self, strs: List[str]) -> str:
+        """Encodes a list of strings to a single string.
+        """
+        encoded = map(lambda word: f"{len(word)}:/{word}", strs)
+
+        return ''.join(encoded)
         
-    def decode(self, s):
-        return s.split('\U0001f926‍♂️')
+
+    def decode(self, s: str) -> List[str]:
+        """Decodes a single string to a list of strings.
+        """
+        decoded = []
+        i = 0
+
+        while i < len(s):
+            delimiter_idx = s.find(':/', i)
+
+            word_len = int(s[i:delimiter_idx])
+            word_start = delimiter_idx + 2
+            word_end = word_start + word_len
+
+            decoded.append(s[word_start:word_end])
+
+            i = word_end
+
+        return decoded
+
+        
+
+        
+
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(strs))
