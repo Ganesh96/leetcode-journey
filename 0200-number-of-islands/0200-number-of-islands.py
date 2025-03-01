7875841c0e17d85ac6d79631ecf7f90d75db3832
GS <ganesan.santhanam96@gmail.com>
2025-02-28 18:31:03 -0600

    Time: 381 ms (5.07%), Space: 34.9 MB (6.12%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,50 +1,45 @@
-class UnionFind:
-    def __init__(self, grid):
-        self.parent = {}
-        self.rank = {}
-        self.count = 0
-        
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == '1':
-                    self.parent[(i, j)] = (i, j)
-                    self.rank[(i, j)] = 1
-                    self.count += 1
-        print(self.parent)
-        print(self.rank)
-    def find(self, x):
-        if self.parent[x] != x:
-            self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
-    
-    def union(self, x, y):
-        rootX = self.find(x)
-        rootY = self.find(y)
-        
-        if rootX != rootY:
-
-            if self.rank[rootX] > self.rank[rootY]:
-                self.parent[rootY] = rootX
-            elif self.rank[rootX] < self.rank[rootY]:
-                self.parent[rootX] = rootY
-            else:
-                self.parent[rootY] = rootX
-                self.rank[rootX] += 1
-            self.count -= 1
 class Solution:
     def numIslands(self,grid):
         if not grid:
             return 0
-        
-        uf = UnionFind(grid)
+
         rows, cols = len(grid), len(grid[0])
-        
+        par = {}
+        rank = {}
+        count = 0
+
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == '1':
+                    par[(i, j)] = (i, j)
+                    rank[(i, j)] = 1
+                    count += 1
+
+        def find(n1):
+            res = n1
+            while res != par[res]:
+                par[res] = par[par[res]]
+                res = par[res]
+            return res
+
+        def union(n1, n2):
+            p1, p2 = find(n1), find(n2)
+            if p1 == p2:
+                return 0
+            if rank[p2] > rank[p1]:
+                par[p1] = p2
+                rank[p2] += rank[p1]
+            else:
+                par[p2] = p1
+                rank[p1] += rank[p2]
+            return 1
+
         for i in range(rows):
             for j in range(cols):
                 if grid[i][j] == '1':
                     for x, y in [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]:
                         if 0 <= x < rows and 0 <= y < cols and grid[x][y] == '1':
-                            uf.union((i, j), (x, y))
-        print(uf.parent)
-        print(uf.rank)
-        return uf.count
+                            if union((i, j), (x, y)):
+                                count -= 1
+
+        return count
