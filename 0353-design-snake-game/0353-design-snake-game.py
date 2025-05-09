5b0123b489f621bbe7a6f04dd4ecb8d6753e16f6
GS <ganesan.santhanam96@gmail.com>
2025-05-09 15:34:16 -0500

    Time: 179 ms (87.19%), Space: 19.5 MB (41.46%) - LeetHub

--- 0353-design-snake-game/0353-design-snake-game.py
+++ 0353-design-snake-game/0353-design-snake-game.py
@@ -1,42 +1,37 @@
-from collections import deque
 class SnakeGame:
 
     def __init__(self, width: int, height: int, food: List[List[int]]):
-        self.w = width
-        self.h = height
+        self.snake = [[0, 0]]
+        self.width = width
+        self.height = height
         self.food = food
-        self.food_index = 0
-        self.points = 0
-
-        self.snake = deque([(0,0)])
-        self.s_set = set([(0,0)])
-        self.cardinals = {
-            'U':(-1,0),
-            'D':(1,0),
-            'R':(0,1),
-            'L':(0,-1)
-        }
+        self.move_map = {"U": [-1, 0], "D": [1, 0], "L": [0, -1], "R": [0, 1]}
+        self.score = 0
 
     def move(self, direction: str) -> int:
-        r,c = self.snake[0]
-        new_r, new_c = self.cardinals[direction]
-        new_r, new_c = new_r+r , new_c+c
-        if not(0<=new_r<self.h and 0<=new_c<self.w):
-            return -1
-        tail = self.snake.pop()
-        self.s_set.remove(tail)
+        r, c = self.move_map[direction]
+        # get the new head and tail
+        head = self.snake[0]
+    
+        new_head = [head[0] + r, head[1] + c]
+        # find the food
+        if len(self.food) > 0 and new_head == self.food[0]:
+            self.food.pop(0)
+            self.score += 1
+        else:
+            # move the tail
+            self.snake.pop()
+            if new_head[0] < 0 or new_head[0] >= self.height or new_head[1] < 0 or new_head[1] >= self.width:
+                return -1
+            elif new_head in self.snake:
+                return -1
+        
+        self.snake.insert(0, new_head)
+        
+        return self.score
+            
 
-        if (new_r,new_c) in self.s_set:
-            return -1
-
-        if(self.food_index < len(self.food) and new_r == self.food[self.food_index][0] and new_c == self.food[self.food_index][1]):
-            self.food_index+=1
-            self.points+=1
-            self.snake.append(tail)
-            self.s_set.add(tail)
-        self.snake.appendleft((new_r,new_c))
-        self.s_set.add((new_r,new_c))
-        return self.points
+            
 
 
 # Your SnakeGame object will be instantiated and called as such:
