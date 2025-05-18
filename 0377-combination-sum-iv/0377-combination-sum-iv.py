3ff3029ebc503df3b51f60f666ed855e6791df7d
GS <ganesan.santhanam96@gmail.com>
2025-05-17 23:41:36 -0500

    Time: 5022 ms (5.6%), Space: 26 MB (6.28%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.py
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -9,20 +9,14 @@
 #         return dp[target]
 
 class Solution:
-    def combinationSum4(self, nums: List[int], target: int, max_len: int = 100) -> int:
-        OFFSET = 10 * max_len
-        dp = [[0] * (max_len + 1) for _ in range(2 * OFFSET + 1)]
-        dp[OFFSET][0] = 1  # sum=0, length=0
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        dp = [[0] * (target + 1) for _ in range(target + 1)]
+        dp[0][0] = 1
         
-        for l in range(1, max_len + 1):
-            for s in range(-OFFSET, OFFSET + 1):
+        for i in range(1, target + 1):
+            for l in range(1, target + 1):
                 for num in nums:
-                    prev = s - num
-                    if -OFFSET <= prev <= OFFSET:
-                        dp[s + OFFSET][l] += dp[prev + OFFSET][l - 1]
+                    if i - num >= 0:
+                        dp[i][l] += dp[i - num][l - 1]
         
-        total = 0
-        for l in range(1, max_len + 1):
-            total += dp[target + OFFSET][l]
-        return total
-
+        return sum(dp[target][1:])
