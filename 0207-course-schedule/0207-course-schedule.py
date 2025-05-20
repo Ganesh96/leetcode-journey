6967635aa967d4bf5b0690f2629032023dc58cf8
GS <ganesan.santhanam96@gmail.com>
2025-05-20 00:29:56 -0400

    Time: 7 ms (47.87%), Space: 19.3 MB (40.09%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,24 +1,32 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        indegree = [0] * numCourses
-        adj = [[] for x in range(numCourses)]
-        
-        for prereq in prerequisites:
-            adj[prereq[1]].append(prereq[0])
-            indegree[prereq[0]] += 1
+        from collections import defaultdict
 
-        queue = []
-        for i in range(numCourses):
-            if indegree[i] == 0:
-                queue.append(i)
-        
-        visited = 0
-        while queue:
-            node = queue.pop(0)
-            visited += 1
-            for neighbor in adj[node]:
-                indegree[neighbor] -= 1
-                if indegree[neighbor] == 0:
-                    queue.append(neighbor)
-        
-        return numCourses == visited
+        graph = defaultdict(list)
+        for a, b in prerequisites:
+            graph[b].append(a)
+
+        visited = [0] * numCourses  # 0=unvisited, 1=visiting, 2=visited
+
+        def dfs(course):
+            # if visiting, we found a cycle
+            if visited[course]==1:
+                return False
+            if visited[course]==2:
+                return True
+            
+            visited[course] = 1  # mark as visiting
+
+            for neighbor in graph[course]:
+                if not dfs(neighbor):
+                    return False
+
+            visited[course] = 2  # mark as fully visited
+            return True
+
+
+        for course in range(numCourses):
+            if visited[course] == 0:
+                if not dfs(course):
+                    return False
+        return True
