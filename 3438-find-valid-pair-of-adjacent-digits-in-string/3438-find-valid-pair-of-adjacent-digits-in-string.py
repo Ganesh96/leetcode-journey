371be5c850f50668b2a9ec6e567210a2cc2503a9
GS <ganesan.santhanam96@gmail.com>
2025-06-22 20:45:43 -0400

    Time: 8 ms (7.13%), Space: 17.8 MB (64.83%) - LeetHub

--- 3438-find-valid-pair-of-adjacent-digits-in-string/3438-find-valid-pair-of-adjacent-digits-in-string.py
+++ 3438-find-valid-pair-of-adjacent-digits-in-string/3438-find-valid-pair-of-adjacent-digits-in-string.py
@@ -1,15 +1,18 @@
+import collections
+
 class Solution:
     def findValidPair(self, s: str) -> str:
-        freq = {}
-        for i in s:
-            if i not in freq:
-                freq[i] = 1
-            else:
-                freq[i] += 1
+        digit_counts = collections.Counter(s)
         for i in range(len(s) - 1):
-            first = s[i]
-            second = s[i + 1]
-            if first != second and freq[first] == int(first) and freq[second] == int(second):
-                return first + second
+            # Get the two adjacent digit characters.
+            d1 = s[i]
+            d2 = s[i+1]
+            
+            condition1_met = (d1 != d2)
+            condition2_met = (digit_counts[d1] == int(d1))
+            condition3_met = (digit_counts[d2] == int(d2))
+            
+            if condition1_met and condition2_met and condition3_met:
+                return d1 + d2
+                
         return ""
-        
