171997f7662cc5a8e137e4af4b7df71cab771295
GS <ganesan.santhanam96@gmail.com>
2025-07-05 19:01:47 -0400

    Time: 23 ms (54.99%), Space: 18.5 MB (39.92%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,24 +1,34 @@
 class Solution:
-    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        big_area = 0
-        rows, cols = len(grid), len(grid[0])
-
-        def dfs(i,j,area)->int:
-            if not (0<=i<rows and 0<=j<cols):
-                return area
-            if grid[i][j]!=1:
-                return area
-            grid[i][j] = 2
-            area+=1
-            area = dfs(i,j+1,area)
-            area = dfs(i+1,j,area)
-            area = dfs(i,j-1,area)
-            area = dfs(i-1,j,area)
-            return area
+    def maxAreaOfIsland(self, grid: list[list[int]]) -> int:
         
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j]==1:
-                    area = dfs(i,j,0)
-                    big_area = max(big_area,area)
-        return big_area
+        if not grid:
+            return 0
+            
+        rows, cols = len(grid), len(grid[0])
+        max_area = 0
+
+        def dfs_get_area(r, c):
+            
+            is_out_of_bounds = r < 0 or r >= rows or c < 0 or c >= cols
+
+            if is_out_of_bounds or grid[r][c] == 0:
+                return 0
+            
+            grid[r][c] = 0
+
+            area = 1
+            area += dfs_get_area(r + 1, c) # Down
+            area += dfs_get_area(r - 1, c) # Up
+            area += dfs_get_area(r, c + 1) # Right
+            area += dfs_get_area(r, c - 1) # Left
+            
+            return area
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    current_island_area = dfs_get_area(r, c)
+
+                    max_area = max(max_area, current_island_area)
+        
+        return max_area
