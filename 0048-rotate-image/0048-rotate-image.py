35d035d1531bbbdea4f2b9242ab69b675e0642f2
GS <ganesan.santhanam96@gmail.com>
2025-10-11 18:58:22 -0400

    Time: 0 ms (100%), Space: 17.7 MB (67.12%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -1,31 +1,29 @@
 class Solution:
-    def rotate(self, matrix: list[list[int]]) -> None:
+    def rotate(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        n = len(matrix)
 
-        # Step 1: Transpose the n x n matrix.
-        # This involves swapping matrix[r][c] with matrix[c][r].
-        # We only need to iterate through the upper triangle of the matrix to
-        # avoid swapping elements back to their original place.
-        for r in range(n):
-        #     # The inner loop can start from 'r' to only touch each pair once.
-            for c in range(r, n):
-        #         # Perform the in-place swap.
-                temp = matrix[r][c]
-                matrix[r][c] = matrix[c][r]
-                matrix[c][r] = temp
-        
-        # At this point, the matrix is transposed.
-        # e.g., [[1,2,3],[4,5,6],[7,8,9]] has become [[1,4,7],[2,5,8],[3,6,9]]
+        left, right = 0, len(matrix)-1
 
-        # Step 2: Reverse each row of the transposed matrix.
-        # Iterate through each row in the matrix.
-        for r in range(n):
-            # Reverse the current row 'matrix[r]' in-place.
-            # Most programming languages provide a built-in, in-place
-            # function to reverse a list or array.
-            matrix[r].reverse()
+        while(left < right):
+            for i in range(right-left):
+
+                top,bottom = left,right
+
+                topLeft = matrix[top][left + i]
+
+                # move bottom left into top left
+                matrix[top][left + i] = matrix[bottom-i][left]
+
+                # move bottom right into bottom left
+                matrix[bottom-i][left] = matrix[bottom][right-i]
+
+                # move top right into bottom right
+                matrix[bottom][right - i] = matrix[top+i][right]
+                
+                # move top left into top right
+                matrix[top + i][right] = topLeft
             
-        # After this step, the rotation is complete. The function doesn't need to return anything.
+            left+=1
+            right-=1
