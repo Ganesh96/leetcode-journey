8721e0e8ca25484fdbb05cf1383bba7c03d8f89d
GS <ganesan.santhanam96@gmail.com>
2025-11-19 22:30:23 -0500

    Time: 12 ms (95.9%), Space: 19.1 MB (65.51%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -2,23 +2,32 @@ from collections import Counter
 
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        counts = Counter(tasks)
-        maxHeap = [-c for c in counts.values()]
+        # counts = Counter(tasks)
+        # maxHeap = [-c for c in counts.values()]
 
-        heapq.heapify(maxHeap)
+        # heapq.heapify(maxHeap)
 
-        time = 0
-        q = deque()
+        # time = 0
+        # q = deque()
 
-        while maxHeap or q:
-            time+=1
+        # while maxHeap or q:
+        #     time+=1
 
-            if maxHeap:
-                cnt = 1 + heapq.heappop(maxHeap)
-                if cnt:
-                    q.append([cnt, time+n])
+        #     if maxHeap:
+        #         cnt = 1 + heapq.heappop(maxHeap)
+        #         if cnt:
+        #             q.append([cnt, time+n])
         
-            if q and q[0][1] == time:
-                heapq.heappush(maxHeap, q.popleft()[0])
+        #     if q and q[0][1] == time:
+        #         heapq.heappush(maxHeap, q.popleft()[0])
     
-        return time
+        # return time
+
+        counts = Counter(tasks)
+
+        M = max(counts.values())
+        C = 0
+        for v in counts.values():
+            C+= 1 if v==M else 0
+
+        return max((M-1)*(n+1) + C,len(tasks))
